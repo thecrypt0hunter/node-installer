2aa6310e24f65e7d58b53aaf26d83862ffc550ec
Hunter <thecryptohunter@protonmail.com>
2019-05-09 09:40:05 +0100

    fix typos

--- install-node.sh
+++ install-node.sh
@@ -5,12 +5,12 @@
 
 read -p "Which Fork (redstone, x42, impleum, city, stratis)? " coin
 
-COINSERVICEINSTALLER="https://raw.githubusercontent.com/thecrypt0hunter/node-installer/master/install-node.sh"
-COINSERVICECONFIG="https://raw.githubusercontent.com/thecrypt0hunter/node-installer/master/config/config-$coin.sh"
+COINSERVICEINSTALLER="https://raw.githubusercontent.com/thecrypt0hunter/node-installer/master/install-coin.sh"
+COINSERVICECONFIG="https://raw.githubusercontent.com/thecrypt0hunter/node-installer/master/config/config-${coin}.sh"
 
 # Install Coins Service
-wget ${COINSERVICEINSTALLER} -O /tmp/install-${coin}.sh
+wget ${COINSERVICEINSTALLER} -O /tmp/install-coin.sh
 wget ${COINSERVICECONFIG} -O /tmp/config-${coin}.sh
 chmod +x /tmp/install-${coin}.sh
-/tmp/install-${coin}.sh -f ${coin}
+/tmp/install-coin.sh -c ${coin}
 
