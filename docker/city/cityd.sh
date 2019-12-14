fccb905505ffa9e13fe4a6cb494d804208102dc6
Hunter <thecryptohunter@protonmail.com>
2019-12-14 19:36:59 +0000

    remove city docker

--- docker/city/cityd.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-export DOTNET_CLI_TELEMETRY_OPTOUT=1
-STAKEPARAMS=""
-if [ -f /var/secure/credentials.sh ]; then
-    source /var/secure/credentials.sh
-    STAKEPARAMS=\"-stake -walletname=\${STAKINGNAME} -walletpassword=\${STAKINGPASSWORD}\"
-fi
-cd /root/citynode
-exec dotnet ./City.Chain.dll -datadir=/root/.citychain -maxblkmem=1 -txindex=1 ${STAKEPARAMS}
